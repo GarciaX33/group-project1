3373b9aea8dcea78fe6ae6a6d5af17521fc64d4f
Daniel J. Garcia <danny.garcia80@hotmail.com>
2017-08-31 09:58:56 -0700

    fix indentation and missing semicolon in scripts back end js file

--- js/scripts-backend-only.js
+++ js/scripts-backend-only.js
@@ -1,5 +1,5 @@
-var nameArray = []
-var costArray = []
+var nameArray = [];
+var costArray = [];
 
 
 
@@ -9,7 +9,7 @@ var costArray = []
       if (check == "") {
         alert("Please enter a name");
         return false;
-      };
+      }
 
       var x = document.getElementById("boxNames");
       nameArray.push(document.getElementById("inputName").value);
@@ -28,7 +28,7 @@ var costArray = []
       if (check == "" || check < 0) {
         alert("Please enter a number greater than 0");
         return false;
-      };
+      }
       var y = document.getElementById("boxCost");
       costArray.push(document.getElementById("inputCost").value);
       y.innerHTML = costArray.join('<br/>');
@@ -50,9 +50,9 @@ var costArray = []
       if (check == "" || check < 0) {
         alert("Please enter a tip equal to or greater than 0");
         return false;
-      };
-      var theTip = ((document.getElementById("inputTip").value))
-      return accounting.toFixed((subtotal * theTip), 2)
+      }
+      var theTip = ((document.getElementById("inputTip").value));
+      return accounting.toFixed((subtotal * theTip), 2);
 
 
     }
@@ -63,30 +63,30 @@ var costArray = []
       if (check == "" || check < 0) {
         alert("Please enter a tax rate equal to or greater than 0");
         return false;
-      };
-      var theTax = ((document.getElementById("inputTax").value))
-      return accounting.toFixed((subtotal * theTax), 2)
+      }
+      var theTax = ((document.getElementById("inputTax").value));
+      return accounting.toFixed((subtotal * theTax), 2);
 
 
     }
 
     function truncate(num, decimal) {
-      decimal = 2
+      decimal = 2;
       return accounting.toFixed(num, decimal);
     }
 
     function splitTheBill(item) {
       priceOfMeal = costArray.reduce(subTotal, 0);
       beforeTipandTax = (costArray.reduce(subTotal, 0) / nameArray.length);
-      priceOfMealSplit = truncate(beforeTipandTax)
-      tipAmount = addTip(beforeTipandTax) / 100
-      taxAmount = addTax(beforeTipandTax) / 100
+      priceOfMealSplit = truncate(beforeTipandTax);
+      tipAmount = addTip(beforeTipandTax) / 100;
+      taxAmount = addTax(beforeTipandTax) / 100;
       grossTotal = truncate(beforeTipandTax + taxAmount + tipAmount);
-      grandTotal = document.getElementById("priceOfMeal").innerHTML = "$" + priceOfMeal
-      document.getElementById("cost-of-food").innerHTML = "$" + priceOfMealSplit
-      document.getElementById("tip").innerHTML = "$" + truncate(tipAmount)
-      document.getElementById("tax").innerHTML = "$" + truncate(taxAmount)
+      grandTotal = document.getElementById("priceOfMeal").innerHTML = "$" + priceOfMeal;
+      document.getElementById("cost-of-food").innerHTML = "$" + priceOfMealSplit;
+      document.getElementById("tip").innerHTML = "$" + truncate(tipAmount);
+      document.getElementById("tax").innerHTML = "$" + truncate(taxAmount);
       document.getElementById("sumTotal").innerHTML = "$" + grossTotal;
 
 
-    };
+    }
